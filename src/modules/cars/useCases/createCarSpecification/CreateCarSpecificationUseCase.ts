8a7bc698c133d78d5105e2678eafe1f23891157a
Jeffsantana <jeffersonsantana2008@hotmail.com>
2021-04-22 16:20:12 -0300

    add upload Car Image

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -2,7 +2,6 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/AppError";
-import { response } from "express";
 import { inject, injectable } from "tsyringe"
 
 interface IRequest {
