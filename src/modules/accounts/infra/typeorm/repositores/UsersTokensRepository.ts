0f224c46e1ede0091272933fc3549ad32d2d091c
Jeffsantana <jeffersonsantana2008@hotmail.com>
2021-05-04 20:08:20 -0300

    finish recovery password

--- src/modules/accounts/infra/typeorm/repositores/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositores/UsersTokensRepository.ts
@@ -12,7 +12,6 @@ class UsersTokensRepository implements IUsersTokensRepository {
         this.repository = getRepository(UserTokens);
     }
 
-
     async create({ expires_date, refresh_token, user_id }: ICreateUserTokenDTO): Promise<UserTokens> {
         const userToken = this.repository.create({
             expires_date,
@@ -31,6 +30,11 @@ class UsersTokensRepository implements IUsersTokensRepository {
 
     async deleteById(id: string): Promise<void> {
         await this.repository.delete({ id })
+    }
+
+    async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+        const userToken = await this.repository.findOne({ refresh_token });
+        return userToken;
     }
 
 }
--- src/shared/infra/http/routes/recoveryPassword.routes.ts
+++ src/shared/infra/http/routes/recoveryPassword.routes.ts
@@ -1,9 +1,12 @@
+import { ResetUserPasswordController } from "@modules/accounts/useCases/resetUserPassword/ResetUserPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 import { Router } from "express";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetUserPasswordController = new ResetUserPasswordController()
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle)
+passwordRoutes.post("/reset", resetUserPasswordController.handle)
 
 export { passwordRoutes }
